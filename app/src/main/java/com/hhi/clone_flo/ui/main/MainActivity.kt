60f78120e53cda08af333781b39d04d2a0922513
hhi <hhanhi5258@gmail.com>
2021-07-06 00:37:54 +0900

    splash 구현

--- app/src/main/java/com/hhi/clone_flo/ui/main/MainActivity.kt
+++ app/src/main/java/com/hhi/clone_flo/ui/main/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hhi.clone_flo.ui.main
 
+import android.os.Bundle
 import com.hhi.clone_flo.R
 import com.hhi.clone_flo.base.BaseActivity
 import com.hhi.clone_flo.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
